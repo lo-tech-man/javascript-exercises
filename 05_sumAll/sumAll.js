a2313df5be454ae887c3faa9baab4f3b0ffb261a
Zsolt Borbély <107408415+lo-tech-man@users.noreply.github.com>
2024-08-08 14:03:40 +0200

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+	if (!Number.isInteger(from) ||
+		!Number.isInteger(to) ||
+		from < 0 ||
+		to < 0) {
+			return "ERROR";
+	} else {
+		let finalResult = 0;
 
+		if (from > to) {
+			for (let i = to; i <= from; i++) {
+				finalResult += i;
+			}
+		} else {
+			for (let i = from; i <= to; i++) {
+				finalResult += i;
+			}
+		}
+		return finalResult;
+	}
 };
 
 // Do not edit below this line
