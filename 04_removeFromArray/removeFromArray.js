4c36f4c97c80fdc70ccb48b0c2fa1c65d2bc3f85
Zsolt Borbély <107408415+lo-tech-man@users.noreply.github.com>
2024-08-09 17:07:35 +0200

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(...theArgs) {
+	const ourArray = theArgs[0];
+
+	for (let i = 1; i < theArgs.length; i++) {
+		let indexOfCurrentArgument = ourArray.indexOf(theArgs[i]);
+		
+		if (indexOfCurrentArgument !== -1) {
+			console.log(`Specified element found at index ${indexOfCurrentArgument}.`);
+			ourArray.splice(indexOfCurrentArgument, 1);
+			i -= 1; // check if there's more than one instance of theArgs[i]
+		}
+	}
+
+	return ourArray;
 };
 
 // Do not edit below this line
