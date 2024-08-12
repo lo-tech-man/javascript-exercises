e777236d1b4f90eba3c11c7796710a11f3b557e5
Zsolt Borbély <107408415+lo-tech-man@users.noreply.github.com>
2024-08-12 14:45:57 +0200

    Implement a more elegant solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,13 @@
-const removeFromArray = function(...theArgs) {
-	const ourArray = theArgs[0];
+const removeFromArray = function(array, ...theArgs) {
+	const newArray = [];
 
-	for (let i = 1; i < theArgs.length; i++) {
-		let indexOfCurrentArgument = ourArray.indexOf(theArgs[i]);
-		
-		if (indexOfCurrentArgument !== -1) {
-			console.log(`Specified element found at index ${indexOfCurrentArgument}.`);
-			ourArray.splice(indexOfCurrentArgument, 1);
-			i -= 1; // check if there's more than one instance of theArgs[i]
+	for (const element of array) {
+		if (theArgs.indexOf(element) === -1) {
+			newArray.push(element);
 		}
 	}
 
-	return ourArray;
+	return newArray;
 };
 
 // Do not edit below this line
